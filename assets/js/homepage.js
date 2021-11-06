aced0e046d4294140084c270fa9b53aa99efb093
leticianardi <leticia.tnardi@gmail.com>
2021-11-06 12:07:01 -0700

    get the current time and change color

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,5 +8,31 @@ $(".saveBtn").on("click", function () {
 });
 
 
+//check matching of current hour and the hour in the block and change color
+function colors() {
+  //get the current hour
+  var momentHour = moment().hours();
+
+  //loop to check blockHour and momentHour
+  $(".time-block").each(function () {
+    var blockHour = parseInt($(this).attr("id"));
+
+    //if statement to check againt the block hour and moment hour
+
+    if (momentHour > blockHour) {
+      $(this).addClass("past");
+    } else if (momentHour === blockHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
+
+colors();
+
 //display the current day
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
